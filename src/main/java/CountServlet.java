680503b539890c97d4fcacb9980fabf586e99f86
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-03 00:28:02 -0600

    finished the count servlet exercise

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name ="CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+    private int count;
+
+    public void init() {
+        count = 0;
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        count++;
+        PrintWriter out = response.getWriter();
+        out.println("<h2 align = \"center\">" + count + "<h2>\n");
+    }
+}
