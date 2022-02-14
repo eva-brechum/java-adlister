616b39eb986cec0274f086f4902878de7c144de1
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-13 22:20:18 -0600

    working on the passwords exercise

--- src/main/java/com/codeup/adlister/util/Password.java
+++ src/main/java/com/codeup/adlister/util/Password.java
@@ -12,4 +12,11 @@ public class Password {
     public static boolean check(String password, String hash) {
         return BCrypt.checkpw(password, hash);
     }
+
+    public static void main(String[] args) {
+//        String jane = "passJane";
+//        String hash1 = hash(jane);
+//        System.out.println(hash1);
+//
+    }
 }
